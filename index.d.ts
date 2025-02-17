74fe900b65d14f736225795ae12badd515049621
xmentory <xmentory>
2025-02-17 13:40:48 +1000

    feat: up

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare module "telegram_bot_network" {
+  type InitBotOptions = { [k: string]: any };
+  export function initBot(options: InitBotOptions): void;
+  export class RabbitMQ {
+    constructor(queue: string);
+  }
+}
